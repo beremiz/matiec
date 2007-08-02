c9fcbfa28ead8583ad415bddf1b097c12e2c11ea
lbessard <none@none>
2007-08-02 17:53:55 +0200

    Changing file header

--- lib/iec_std_lib_generated.h
+++ lib/iec_std_lib_generated.h
@@ -1,14 +1,14 @@
 /*
  * (c) 2003 Mario de Sousa
  *
- * Offered to the public under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
+ * Offered to the public under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
  *
  * This code is made available on the understanding that it will not be
  * used in safety-critical situations without a full and competent review.
