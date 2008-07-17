65dfd7c4aaa00e4de3ac8ae921346bd64dbc4d17
etisserant <none@none>
2008-07-17 19:06:53 +0200

    removed __print statements. Not supposed to be in std lib.

--- lib/iec_std_lib.h
+++ lib/iec_std_lib.h
@@ -31,27 +31,6 @@
 
 #include "iec_std_lib_generated.h"
 
-#define __print_BOOL(name) printf("  %s = (BOOL) %s\n",#name, name?"TRUE":"FALSE");
-#define __print_SINT(name) printf("  %s = (SINT) %d\n",#name, name);
-#define __print_INT(name) printf("  %s = (INT) %d\n",#name, name);
-#define __print_DINT(name) printf("  %s = (DINT) %d\n",#name, name);
-#define __print_LINT(name) printf("  %s = (LINT) %d\n",#name, name);
-#define __print_USINT(name) printf("  %s = (USINT) %u\n",#name, name);
-#define __print_UINT(name) printf("  %s = (UINT) %u\n",#name, name);
-#define __print_UDINT(name) printf("  %s = (UDINT) %u\n",#name, name);
-#define __print_ULINT(name) printf("  %s = (ULINT) %lu\n",#name, name);
-#define __print_REAL(name) printf("  %s = (REAL) %f\n",#name, (double)name);
-#define __print_LREAL(name) printf("  %s = (LREAL) %f\n",#name, (double)name);
-#define __print_TIME(name) {STRING __tmp = __time_to_string(name);__tmp.body[__tmp.len] = 0; printf("  %s = (TIME) %*s\n",#name, __tmp.len, &__tmp.body);}
-#define __print_DATE(name) {STRING __tmp = __date_to_string(name);__tmp.body[__tmp.len] = 0; printf("  %s = (DATE) %*s\n",#name, __tmp.len, &__tmp.body);}
-#define __print_TOD(name) {STRING __tmp = __tod_to_string(name);__tmp.body[__tmp.len] = 0; printf("  %s = (TOD) %*s\n",#name, __tmp.len, &__tmp.body);}
-#define __print_DT(name) {STRING __tmp = __dt_to_string(name);__tmp.body[__tmp.len] = 0; printf("  %s = (DT) %*s\n",#name, __tmp.len, &__tmp.body);}
-#define __print_STRING(name) printf("  %s = (STRING) {%d, \"%*s\"}\n",#name, name.len, name.len, &name.body);
-#define __print_BYTE(name) printf("  %s = (BYTE) 0x%2.2x\n",#name, name);
-#define __print_WORD(name) printf("  %s = (WORD) 0x%4.4x\n",#name, name);
-#define __print_DWORD(name) printf("  %s = (DWORD) 0x%8.8x\n",#name, name);
-#define __print_LWORD(name) printf("  %s = (LWORD) 0x%16.16lx\n",#name, name);
-
 #ifdef DEBUG_IEC
 #define DBG(...) printf(__VA_ARGS__);
 #define DBG_TYPE(TYPENAME, name) __print_##TYPENAME(name);
