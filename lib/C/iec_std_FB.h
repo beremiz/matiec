7ff678fc29e25cf178497d11fc086e569681116f
Edouard Tisserant <none@none>
2021-10-04 11:29:13 +0200

    Remove RETAIN qualifier for F_TRIG and R_TRIG 'M' variable. This is not following standard, and implicitely consumes a lot of RETAIN memory. User can explicitely qualify R_TRIG of F_TRIG instance as RETAIN if needed.

--- lib/C/iec_std_FB.h
+++ lib/C/iec_std_FB.h
@@ -565,7 +565,7 @@ static void R_TRIG_init__(R_TRIG *data__, BOOL retain) {
   __INIT_VAR(data__->ENO,__BOOL_LITERAL(TRUE),retain)
   __INIT_VAR(data__->CLK,__BOOL_LITERAL(FALSE),retain)
   __INIT_VAR(data__->Q,__BOOL_LITERAL(FALSE),retain)
-  __INIT_VAR(data__->M,__BOOL_LITERAL(FALSE),1)
+  __INIT_VAR(data__->M,__BOOL_LITERAL(FALSE),retain)
 }
 
 // Code part
@@ -598,7 +598,7 @@ static void F_TRIG_init__(F_TRIG *data__, BOOL retain) {
   __INIT_VAR(data__->ENO,__BOOL_LITERAL(TRUE),retain)
   __INIT_VAR(data__->CLK,__BOOL_LITERAL(FALSE),retain)
   __INIT_VAR(data__->Q,__BOOL_LITERAL(FALSE),retain)
-  __INIT_VAR(data__->M,__BOOL_LITERAL(FALSE),1)
+  __INIT_VAR(data__->M,__BOOL_LITERAL(FALSE),retain)
 }
 
 // Code part
