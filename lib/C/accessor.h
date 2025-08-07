bd624e92f907d019584b769d5201d8b978f0a163
Edouard Tisserant <edouard.tisserant@gmail.com>
2025-08-08 00:40:48 +0200

    One more consequence of "Add _data__ suffix to POUs Data Structure types."

--- lib/C/accessor.h
+++ lib/C/accessor.h
@@ -24,7 +24,7 @@
 	type##_data__* __GET_GLOBAL_##name(void) {\
 		return &(*GLOBAL__##name);\
 	}\
-	extern void type##_init__(type* data__, BOOL retain);
+	extern void type##_init__(type##_data__* data__, BOOL retain);
 #define __DECLARE_GLOBAL_LOCATION(type, location)\
 	extern type *location;
 #define __DECLARE_GLOBAL_LOCATED(type, resource, name)\
